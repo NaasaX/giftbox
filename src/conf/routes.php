2bc86e5ea45b0f471851ede66fc3e23c9d9d0d1c
cemquo <semihfeyyaztopcu@gmail.com>
2025-05-21 16:32:40 +0200

    Exercice 5, page d'accueil et coffrets

--- src/conf/routes.php
+++ src/conf/routes.php
@@ -1,6 +1,9 @@
 <?php
 declare(strict_types=1);
 
+use gift\actions\GetCoffretAction;
+use gift\actions\GetCoffretsAction;
+use gift\actions\GetHomePageAction;
 use Slim\App;
 use gift\actions\GetCategoriesAction;
 use gift\actions\GetCategorieAction;
@@ -11,10 +14,7 @@ use \Psr\Http\Message\ResponseInterface as Response;
 return function(App $app): App {
 
   // Route 0 : GET /
-  $app->get('/', function(Request $request, Response $response) {
-      $response->getBody()->write("Bienvenue sur l'application");
-      return $response;
-  });
+  $app->get('/', GetHomePageAction::class);
 
   // Route 1 : GET /categories
   $app->get('/categories', GetCategoriesAction::class);
@@ -24,6 +24,11 @@ return function(App $app): App {
 
   // Route 3 : GET /prestation?id=xxxx
   $app->get('/prestation', GetPrestationAction::class);
+
+  // Route 4 : GET /coffrets
+  $app->get('/coffrets', GetCoffretsAction::class);
+
+  $app->get('/coffrets/{id}', GetCoffretAction::class);
 
   return $app;
 };
