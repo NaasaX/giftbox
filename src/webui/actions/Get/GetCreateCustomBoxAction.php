8debc5e9d6fc248c175cb3ee091c482bb1cd3336
ClemDW <clementdewasch54@gmail.com>
2025-06-04 14:56:17 +0200

    feat: csrf part1

---
+++ src/webui/actions/Get/GetCreateCustomBoxAction.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Giftbox\webui\actions\Get;
+
+use Giftbox\providers\CsrfTokenProvider;
+use Giftbox\ApplicationCore\Domain\Entities\Prestation;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Exception\HttpBadRequestException;
+use Slim\Views\Twig;
+
+class GetCreateCustomBoxAction
+{
+    public function __invoke(Request $request, Response $response, array $args): Response
+    {
+        try {
+            $prestations = Prestation::with('categorie')->get();
+
+            // Grouper les prestations par catégorie
+            $prestationsParCategorie = [];
+            foreach ($prestations as $presta) {
+                $cat = $presta->categorie->libelle ?? 'Autres';
+                $prestationsParCategorie[$cat][] = $presta;
+            }
+
+            //On génère le token Csrf
+            $csrfToken = CsrfTokenProvider::generate();
+
+            return Twig::fromRequest($request)->render($response, 'createCustomBox.twig', [
+                'prestationsParCategorie' => $prestationsParCategorie,
+                'csrf_token' => $csrfToken
+            ]);
+
+        } catch (\InvalidArgumentException $e) {
+            throw new HttpBadRequestException($request, $e->getMessage());
+
+        } catch (\Exception $e) {
+            $response->getBody()->write("Une erreur est survenue" . $e->getMessage());
+            return $response->withStatus(500);
+        }
+    }
+}
